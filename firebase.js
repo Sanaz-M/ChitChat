609f362108a16c22ccbe641d86a89e56a7f30285
Sanaz M <sanazmohammaddi@gmail.com>
2023-09-13 14:43:21 +0100

    set the firebase configs

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUuFZ8lHm1J1RZ46omsh8jrNMD6b5cmk8",
+  authDomain: "chichat-app-a9a8e.firebaseapp.com",
+  databaseURL: "https://chichat-app-a9a8e-default-rtdb.firebaseio.com",
+  projectId: "chichat-app-a9a8e",
+  storageBucket: "chichat-app-a9a8e.appspot.com",
+  messagingSenderId: "281233578815",
+  appId: "1:281233578815:web:3acec1990758f46c25918a"
+};
